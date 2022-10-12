159a15a4c3b641c7dce6c9af68cb1953c3ccae0c
Isa <izbagov0133@gmail.com>
2022-10-12 20:47:58 +0300

    Updated Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,46 +1,72 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
 import "./Form.css";
 
 function Form() {
-    const [country, setCountry] = useState();
-    const [street, setStreet] = useState();
-    const [subject, setSubject] = useState();
-    
-    const {tg} = useTelegram();
+  const [country, setCountry] = useState();
+  const [street, setStreet] = useState();
+  const [subject, setSubject] = useState();
 
-    useEffect(() => {
-        tg.MainButton.setParams({
-            text: 'Отправить данные',
-        })
-    }, [tg]);
+  const { tg } = useTelegram();
 
-    useEffect(() => {
-        if (!street || !country) {
-            tg.MainButton.hide();
-        }
-    }, [street, country, tg]);
+  const onSendData = useCallback(() => {
+    const data = {
+        country,
+        street,
+        subject,
+    }; 
+    tg.sendData(JSON.stringify(data));
+  }, [country, street, subject, tg]);
 
-    function onChangeCountry(e) {
-        setCountry(e.target.value);
+  useEffect(() => {
+    tg.WebApp.onEvent("mainButtonClicked", onSendData);
+    return tg.WebApp.offEvent("mainButtonClicked", onSendData);
+  }, [tg, onSendData]);
+
+  useEffect(() => {
+    tg.MainButton.setParams({
+      text: "Отправить данные",
+    });
+  }, [tg]);
+
+  useEffect(() => {
+    if (!street || !country) {
+      tg.MainButton.hide();
     }
+  }, [street, country, tg]);
 
-    function onChangeStreet(e) {
-        setStreet(e.target.value);
-    } 
+  function onChangeCountry(e) {
+    setCountry(e.target.value);
+  }
 
-    function onChangeSubject(e) {
-        setSubject(e.target.value);
-    } 
+  function onChangeStreet(e) {
+    setStreet(e.target.value);
+  }
+
+  function onChangeSubject(e) {
+    setSubject(e.target.value);
+  }
 
   return (
     <div className="form">
       <h3>Введите ваши данные</h3>
-      <input className="input" type="text" placeholder="Страна" value={country} onChange={onChangeCountry}/>
-      <input className="input" type="text" placeholder="Улица" value={street} onChange={onChangeStreet}/>
+      <input
+        className="input"
+        type="text"
+        placeholder="Страна"
+        value={country}
+        onChange={onChangeCountry}
+      />
+      <input
+        className="input"
+        type="text"
+        placeholder="Улица"
+        value={street}
+        onChange={onChangeStreet}
+      />
       <select className="select" value={subject} onChange={onChangeSubject}>
-        <option value='physical'>Физ. лицо</option>
-        <option value='legal'>Юр. лицо</option>
+        <option value="physical">Физ. лицо</option>
+        <option value="legal">Юр. лицо</option>
       </select>
     </div>
   );
